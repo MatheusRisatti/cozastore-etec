83ef0cccd483d7a3cb3327f78ab4e20510e97326
MatheusRisatti <risattimatheus@gmail.com>
2024-02-19 08:50:25 -0300

    cria as classes cor e tamanho

---
+++ Cozastore/Models/Cor.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cozastore.Models;
+
+[Table("Cor")]
+public class Cor
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Digite o nome porfa")]
+    [StringLength(30, ErrorMessage = "o Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [Display(Name = "Código Hexa", Prompt = "Ex. #000000")]
+    [Required(ErrorMessage = "Informe por favor, o Código Hexa")]
+    [StringLength(7, ErrorMessage = "O código Hexa deve possuir no maximo 7 caracteres")]
+    public string CodigoHexa { get; set; }
+}
---
+++ Cozastore/Models/Tamanho.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cozastore.Models;
+
+[Table ("Tamanho")]
+    public class Tamanho
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "porfavor, informe e goze a Sigla")]
+        [StringLength(5, ErrorMessage = "A Sigla deve possuir no máximo 5 caracteres")]
+        public string Sigla { get; set; }
+    
+        [Required(ErrorMessage = "Digite o nome porfa")]
+        [StringLength(30, ErrorMessage = "o Nome deve possuir no máximo 30 caracteres")]
+        public string Nome { get; set; }
+    }
